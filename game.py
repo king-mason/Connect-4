2750a07b4fb0e4f017f20c19ba64c73ad84c44b0
Mason <masonelephant@gmail.com>
2023-06-15 20:50:05 -0700

    Menu updates and visual bug fixes

--- game.py
+++ game.py
@@ -9,13 +9,14 @@ WIDTH, HEIGHT = (1000, 800)
 COL_WIDTH = WIDTH / COLS
 
 COLOR_1 = 'cyan'
-COLOR_2 = 'darkorchid'
+COLOR_2 = 'dark orchid'
 COLOR_BG = 'white'
 COLOR_BOARD = (255, 225, 0, 255)  # yellow
 COLOR_MENU = 'yellow'
 COLOR_TEXT = 'dark blue'
-COLOR_BUTTON = (0, 0, 255, 255)  # blue
-COLOR_BUTTON_HOVER = (100, 100, 100, 100)  # gray
+COLOR_BUTTON = 'blue'
+COLOR_BUTTON_HOVER = 'purple'
+COLOR_BUTTON_SELECTED = 'dark orchid 4'
 
 COLOR_HIGHLIGHT_KEYS = {1: COLOR_1, 2: COLOR_2}
 
@@ -32,10 +33,23 @@ BUTTON_OPTIONS = Button(text_input='OPTIONS', pos=(WIDTH / 2, HEIGHT / 2 + 200),
 BUTTON_QUIT = Button(text_input='QUIT', pos=(WIDTH / 2, HEIGHT / 2 + 260),
                      font=FONT_MEDIUM, base_color=COLOR_BUTTON,
                      hover_color=COLOR_BUTTON_HOVER)
-BUTTON_MODE = None
+BUTTON_MODE = Button(text_input='MODE', pos=(WIDTH / 2, HEIGHT / 2 - 200),
+                     font=FONT_MEDIUM, base_color=COLOR_BUTTON,
+                     hover_color=COLOR_BUTTON_HOVER)
+BUTTON_MODE_1 = Button(text_input='TWO PLAYER', pos=(WIDTH / 2, HEIGHT / 2 - 200),
+                       font=FONT_MEDIUM, base_color=COLOR_BUTTON,
+                       hover_color=COLOR_BUTTON_HOVER)
+BUTTON_MODE_2 = Button(text_input='ONE PLAYER', pos=(WIDTH / 2, HEIGHT / 2 - 100),
+                       font=FONT_MEDIUM, base_color=COLOR_BUTTON,
+                       hover_color=COLOR_BUTTON_HOVER)
+BUTTON_MODE_3 = Button(text_input='AUTO', pos=(WIDTH / 2, HEIGHT / 2),
+                       font=FONT_MEDIUM, base_color=COLOR_BUTTON,
+                       hover_color=COLOR_BUTTON_HOVER)
 BUTTON_DIFFICULTY = None
 BUTTON_BOARD = None
-
+BUTTON_BACK = Button(text_input='BACK', pos=(WIDTH - 150, HEIGHT - 100),
+                     font=FONT_SMALL, base_color=COLOR_BUTTON,
+                     hover_color=COLOR_BUTTON_HOVER)
 
 NUM_KEYS = range(48, 58)
 
@@ -43,8 +57,6 @@ MODE = 2  # 1: pvp, 2: pvc, 3: cvc
 
 DROP_DELAY = 100
 DELAY = DROP_DELAY * ROWS
-
-winners = {1: 0, 2: 0}
 
 
 class Connect4:
@@ -58,6 +70,7 @@ class Connect4:
         self.cols = COLS
         self.game_board = Board(ROWS, COLS)
         self.draw_board()
+        self.mode = 1
         self.turn = 1
         self.piece_row = -1
         self.piece_col = -1
@@ -77,6 +90,8 @@ class Connect4:
                 self.play()
             if self.state == 'Options':
                 self.options()
+            if self.state == 'Mode Select':
+                self.mode_selector()
             if self.state == 'Board Select':
                 self.board_selector()
             if self.state == 'Difficulty Select':
@@ -95,27 +110,42 @@ class Connect4:
 
     def options(self):
         self.screen.fill(COLOR_MENU)
-        for button in []:
+        for button in [BUTTON_MODE, BUTTON_BACK]:
+            button.check_hover(self.mouse_pos)
+            button.update(self.screen)
+
+    def mode_selector(self):
+        self.screen.fill(COLOR_MENU)
+        for i, button in enumerate([BUTTON_MODE_1, BUTTON_MODE_2, BUTTON_MODE_3, BUTTON_BACK]):
+            if self.mode == i + 1:
+                button.base_color = COLOR_BUTTON_SELECTED
+                button.selected = True
+            else:
+                button.base_color = COLOR_BUTTON
+                button.selected = False
             button.check_hover(self.mouse_pos)
             button.update(self.screen)
 
     def board_selector(self):
+        self.screen.fill(COLOR_MENU)
         for button in []:
             button.check_hover(self.mouse_pos)
             button.update(self.screen)
-        self.screen.fill(COLOR_MENU)
 
     def difficulty_selector(self):
         self.screen.fill(COLOR_MENU)
+        for button in []:
+            button.check_hover(self.mouse_pos)
+            button.update(self.screen)
 
     def play(self):
         self.draw_board()
         if pygame.time.get_ticks() >= self.delay_start + DELAY and not self.falling:
-            if MODE == 1:
+            if self.mode == 1:
                 self.player_vs_player()
-            if MODE == 2:
+            if self.mode == 2:
                 self.player_vs_computer()
-            if MODE == 3:
+            if self.mode == 3:
                 self.computer_vs_computer()
 
     def player_vs_player(self):
@@ -141,13 +171,9 @@ class Connect4:
                     return
                 self.piece_row = 0
                 self.falling = True
-                winner = self.game_board.check_win()
-                if winner:
-                    pygame.time.wait(2000)
-                    self.game_board.reset_board()
-                self.delay_start = pygame.time.get_ticks()
 
         else:
+            pygame.display.flip()
             new_board, _ = minimax(self.game_board, 0, True, -np.inf, np.inf)
             diff = new_board.board - self.game_board.board
             for col in range(COLS):
@@ -171,6 +197,18 @@ class Connect4:
             self.falling = True
             self.delay_start = pygame.time.get_ticks()
 
+    def go_back(self):
+        if self.state == 'Play':
+            self.state = 'Menu'
+        if self.state == 'Options':
+            self.state = 'Menu'
+        if self.state == 'Mode Select':
+            self.state = 'Options'
+        if self.state == 'Board Select':
+            self.state = 'Options'
+        if self.state == 'Difficulty Select':
+            self.state = 'Options'
+
     def reset(self):
         self.turn = 1
         self.piece_row = -1
@@ -187,7 +225,7 @@ class Connect4:
             for j in range(COLS):
                 w, h = WIDTH // COLS, HEIGHT // ROWS
                 # Create a circle for each space
-                circle_rect = pygame.rect.Rect(w * j + 10, h * i + 10, w - 20, h - 20)  
+                circle_rect = pygame.rect.Rect(w * j + 10, h * i + 10, w - 20, h - 20)
                 if self.game_board.board[i, j] == 1:
                     color = COLOR_1
                 elif self.game_board.board[i, j] == 2:
@@ -206,20 +244,18 @@ class Connect4:
     def handle_inputs(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                print(winners)
                 quit()
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    print(winners)
-                    self.state = 'Menu'
+                    self.go_back()
                     self.reset()
                 if event.key in NUM_KEYS:
                     if pygame.time.get_ticks() >= self.delay_start + DELAY:
                         # Set column to key number
                         self.piece_col = event.key - 49
                 if event.key == pygame.K_SPACE:
-                    print(winners)
+                    print(self.game_board.get_id())
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if self.state == 'Menu':
@@ -232,12 +268,24 @@ class Connect4:
                     if BUTTON_QUIT.check_hover(self.mouse_pos):
                         quit()
 
-                if self.state == 'Options':
-                    pass
+                elif self.state == 'Options':
+                    if BUTTON_MODE.check_hover(self.mouse_pos):
+                        self.state = 'Mode Select'
 
-                if self.state == 'Play':
+                elif self.state == 'Mode Select':
+                    if BUTTON_MODE_1.check_hover(self.mouse_pos):
+                        self.mode = 1
+                    if BUTTON_MODE_2.check_hover(self.mouse_pos):
+                        self.mode = 2
+                    if BUTTON_MODE_3.check_hover(self.mouse_pos):
+                        self.mode = 3
+
+                elif self.state == 'Play':
                     if pygame.time.get_ticks() >= self.delay_start + DELAY and not self.falling:
                         self.piece_col = int(self.mouse_pos[0] // COL_WIDTH)
+
+                if BUTTON_BACK.check_hover(self.mouse_pos):
+                    self.go_back()
 
             # Falling animation
             if event.type == self.drop_timer:
@@ -264,6 +312,15 @@ class Connect4:
                         winner = self.game_board.check_win()
                         if winner:
                             self.draw_board()
+                            pygame.display.flip()
+                            pygame.time.wait(2000)
+                            self.game_board.reset_board()
+
+                        # Check for draw
+                        empty_spaces = (self.game_board.board == 0).sum()
+                        if empty_spaces == 0:
+                            self.draw_board()
+                            pygame.display.flip()
                             pygame.time.wait(2000)
                             self.game_board.reset_board()
 
